61697e7c7d421904eab3a20bb8fbe68c7a995652
HangHuynh1998 <huynhhang.24hdev@gmail.com>
2020-11-20 01:10:57 +0700

    add config api

---
+++ app/services/index.js
@@ -0,0 +1,19 @@
+const userService = require("./users.service");
+const authService = require("./auth.service");
+const toursService = require("./tours.service");
+const companyService = require("./company.service");
+const customerService = require("./customer.service");
+const applicationService = require("./application.service");
+const categoryService = require("./categories.service");
+const contactService = require("./contact.service");
+
+module.exports = {
+    userService,
+    authService,
+    toursService,
+    companyService,
+    customerService,
+    applicationService,
+    categoryService,
+    contactService
+}
